6e2890a1e57939bbfd2be9ebe100858d6583e123
LAGNIKA Fadel <adissalagnika23@gmail.com>
2019-10-11 09:54:50 +0100

    a

--- example.php
+++ example.php
@@ -115,31 +115,14 @@
                 <?php
                     $attendance = $zk->getAttendance();
                     ?>
-                    <?php if (count($attendance) > 0) {
-                        $attendance = array_reverse($attendance, true);
-                        sleep(1);
-                        foreach ($attendance as $attItem) {
-                            ?>
-                            <tr>
-                                <td><?php echo($attItem['uid']); ?></td>
-                                <td><?php echo($attItem['id']); ?></td>
-                                <td><?php echo(isset($users[$attItem['id']]) ? $users[$attItem['id']]['name'] : $attItem['id']); ?></td>
-                                <td><?php echo(ZK\Util::getAttState($attItem['state'])); ?></td>
-                                <td><?php echo(date("d-m-Y", strtotime($attItem['timestamp']))); ?></td>
-                                <td><?php echo(date("H:i:s", strtotime($attItem['timestamp']))); ?></td>
-                                <td><?php echo(ZK\Util::getAttType($attItem['type'])); ?></td>
-                            </tr>
-                            <?php
-                        }
-                    }
-                ?>
             </table>
+            <?php var_dump($attendance) ?>
             <?php
                 if (count($attendance) > 0) {
                     //$zk->clearAttendance(); // Remove attendance log only if not empty
                 }
             ?>
-        <?php } ?>*/?>
+        <?php } ?>
         <?php
         $zk->enableDevice();
         $zk->disconnect();
